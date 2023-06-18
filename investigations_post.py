0c0fdf1cfd3eb15854fa22f3415586cf02b0fb52
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2023-06-17 22:01:00 -0500

    modified:   investigations_post.py

--- investigations_post.py
+++ investigations_post.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
     for key in config:
         clients.append(key)
     for client in clients:
-        with open("config.json", "r", encoding="UTF-8") as config_file:
-            config = json.load(config_file)
-            if config[client]["enabled"] is True:
-                investigation_post(client)
-            else:
-                print(str(client) + " is Disabled.")
+        if config[client]["enabled"] is True:
+            investigation_post(client)
+        else:
+            print(str(client) + " is Disabled.")

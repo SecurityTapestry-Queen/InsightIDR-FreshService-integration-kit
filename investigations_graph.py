9b043489f957c271357e97eebe8694fea9c48676
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2023-09-07 11:36:55 -0500

    new bargraph + rebase

--- investigations_graph.py
+++ investigations_graph.py
@@ -18,6 +18,7 @@ ORG_ID = {
     "ICS": "593f982d-7eb4-4e39-8b14-83912443326b"
     }
 
+
 def check_investigations():
     '''Checks Investigations from InsightIDR'''
     url = "https://us2.api.insight.rapid7.com/idr/v2/investigations/"
@@ -28,17 +29,31 @@ def check_investigations():
         "size": 100, #Default 20
         "start_time": "2022-10-18T00:00:00Z" #Default Start Time 28 days prior
         }
-    num_pages = requests.get(url, params, headers=headers, timeout=30).json()['metadata']['total_pages']
+    try:
+        num_pages = requests.get(url, params, headers=headers, timeout=30).json()['metadata']['total_pages']
+    except KeyError:
+        print("Error obtaining Page Metadata")
+    
     investigations_list = []
+    
     lab_list = []
     hssd_list = []
     lom_list = []
     gosm_list = []
     ics_list = []
 
+    unspecified_list = []
+    low_list = []
+    medium_list = []
+    high_list = []
+    critical_list = []
+
     for page in range(num_pages):
         params['index'] = page
-        request = requests.get(url, params, headers=headers, timeout=30).json()['data']
+        try:
+            request = requests.get(url, params, headers=headers, timeout=30).json()['data']
+        except KeyError:
+            print("Error requesting Page "+str(page)+" Data")
         investigations_list.extend(request)
         page += 1
 
@@ -54,13 +69,103 @@ def check_investigations():
         if investigation['organization_id'] == ORG_ID['ICS']:
             ics_list.append(investigation)
 
-    # TESTING1 - Dump to file for testing
-    json_dump = json.dumps(investigations_list, indent=4)
-    with open("investigations_list.json","w",encoding="UTF-8") as outfile:
-        outfile.write(json_dump)
-    # END TESTING1
+    for investigation in investigations_list:
+        if investigation['priority'] == "UNMAPPED":
+            unspecified_list.append(investigation)
+        if investigation['priority'] == "LOW":
+            low_list.append(investigation)
+        if investigation['priority'] == "MEDIUM":
+            medium_list.append(investigation)
+        if investigation['priority'] == "HIGH":
+            high_list.append(investigation)
+        if investigation['priority'] == "CRITICAL":
+            critical_list.append(investigation)
 
-    return investigations_list, lab_list, hssd_list, lom_list, gosm_list, ics_list
+    investigations_dump = json.dumps(investigations_list, indent=4)
+    with open("investigations_json/investigations_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(investigations_dump)
+
+    lab_dump = json.dumps(lab_list, indent=4)
+    with open("investigations_json/lab_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(lab_dump)
+    
+    hssd_dump = json.dumps(hssd_list, indent=4)
+    with open("investigations_json/hssd_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(hssd_dump)
+
+    lom_dump = json.dumps(lom_list, indent=4)
+    with open("investigations_json/lom_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(lom_dump)
+
+    gosm_dump = json.dumps(gosm_list, indent=4)
+    with open("investigations_json/gosm_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(gosm_dump)
+
+    ics_dump = json.dumps(ics_list, indent=4)
+    with open("investigations_json/ics_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(ics_dump)
+
+    unspecified_dump = json.dumps(unspecified_list, indent=4)
+    with open("investigations_json/unspecified_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(unspecified_dump)
+
+    low_dump = json.dumps(low_list, indent=4)
+    with open("investigations_json/low_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(low_dump)
+
+    medium_dump = json.dumps(medium_list, indent=4)
+    with open("investigations_json/medium_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(medium_dump)
+
+    high_dump = json.dumps(high_list, indent=4)
+    with open("investigations_json/high_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(high_dump)
+
+    critical_dump = json.dumps(critical_list, indent=4)
+    with open("investigations_json/critical_list.json","w",encoding="UTF-8") as outfile:
+        outfile.write(critical_dump)
+
+
+def load_investigations():
+    '''Loads Investigations from Ephemeral File'''
+    with open("investigations_json/investigations_list.json","r",encoding="UTF-8") as infile:
+        investigations_loaded = json.load(infile)
+
+    with open("investigations_json/lab_list.json","r",encoding="UTF-8") as infile:
+        lab_loaded = json.load(infile)
+
+    with open("investigations_json/hssd_list.json","r",encoding="UTF-8") as infile:
+        hssd_loaded = json.load(infile)
+
+    with open("investigations_json/lom_list.json","r",encoding="UTF-8") as infile:
+        lom_loaded = json.load(infile)
+
+    with open("investigations_json/gosm_list.json","r",encoding="UTF-8") as infile:
+        gosm_loaded = json.load(infile)
+
+    with open("investigations_json/ics_list.json","r",encoding="UTF-8") as infile:
+        ics_loaded = json.load(infile)
+
+    with open("investigations_json/unspecified_list.json","r",encoding="UTF-8") as infile:
+        unspecified_loaded = json.load(infile)
+
+    with open("investigations_json/low_list.json","r",encoding="UTF-8") as infile:
+        low_loaded = json.load(infile)
+
+    with open("investigations_json/medium_list.json","r",encoding="UTF-8") as infile:
+        medium_loaded = json.load(infile)
+
+    with open("investigations_json/high_list.json","r",encoding="UTF-8") as infile:
+        high_loaded = json.load(infile)
+
+    with open("investigations_json/critical_list.json","r",encoding="UTF-8") as infile:
+        critical_loaded = json.load(infile)
+
+    return (
+        investigations_loaded,
+        lab_loaded,hssd_loaded,lom_loaded,gosm_loaded,ics_loaded,
+        unspecified_loaded,low_loaded,medium_loaded,high_loaded,critical_loaded
+    )
 
 
 def make_autopct(values):
@@ -72,15 +177,35 @@ def make_autopct(values):
     return my_autopct
 
 
-def investigations_pie_chart_all():
-    '''Creates Pie Chart based on incoming Data'''
-    investigations, lab_i, hssd_i, lom_i, gosm_i, ics_i = check_investigations()
+def client_counts():
+    '''Count by Client'''
+    investigations, lab_i, hssd_i, lom_i, gosm_i, ics_i, unspec_i,low_i,medium_i,high_i,critical_i = load_investigations()
     lab_count = len(lab_i)
     hssd_count = len(hssd_i)
     lom_count = len(lom_i)
     gosm_count = len(gosm_i)
     ics_count = len(ics_i)
 
+    return ( investigations, lab_i, hssd_i, lom_i, gosm_i, ics_i,
+            lab_count, hssd_count, lom_count, gosm_count, ics_count )
+
+
+def priority_counts():
+    '''Count by Priority'''
+    investigations, lab_i, hssd_i, lom_i, gosm_i, ics_i, unspec_i,low_i,medium_i,high_i,critical_i  = load_investigations()
+    unspec_count = len(unspec_i)
+    low_count = len(low_i)
+    medium_count = len(medium_i)
+    high_count = len(high_i)
+    critical_count = len(critical_i)
+
+    return ( investigations, unspec_i, low_i, medium_i, high_i, critical_i,
+            unspec_count, low_count, medium_count, high_count, critical_count )
+
+
+def investigations_pie_chart_all_clients():
+    '''Creates Pie Chart based on incoming Data'''
+    investigations, lab_i, hssd_i, lom_i, gosm_i, ics_i, lab_count, hssd_count, lom_count, gosm_count, ics_count = client_counts()
     client_labels = ["Lab","HSSD","LOM","GosM","ICS"]
     client_numbers = [lab_count,hssd_count,lom_count,gosm_count,ics_count]
     plt.figure(figsize=(10,7))
@@ -91,5 +216,22 @@ def investigations_pie_chart_all():
     plt.savefig("./docs/charts/investigations_all.png")
 
 
+def investigations_bar_chart_all_priority():
+    '''Creates Bar Chart based on incoming Data'''
+    investigations, unspec_i, low_i, medium_i, high_i, critical_i, unspec_count, low_count, medium_count, high_count, critical_count = priority_counts()
+    fig, ax = plt.subplots()
+    investigation_labels = ["Unspecified","Low","Medium","High","Critical"]
+    investigation_counts = [unspec_count,low_count,medium_count,high_count,critical_count]
+    investigation_colors = ["seashell","yellow","gold","orange","orangered"]
+
+    bars = ax.bar(investigation_labels, investigation_counts, color=investigation_colors)
+    ax.bar_label(bars, fmt='{:,.0f}')
+    plt.suptitle("Investigations by Priority")
+    plt.title(str(len(investigations))+" Total (Since 10-18-2022)")
+    plt.savefig("./docs/charts/investigations_priority.png")
+
+
 if __name__ == "__main__":
-    investigations_pie_chart_all()
+    check_investigations()
+    investigations_pie_chart_all_clients()
+    investigations_bar_chart_all_priority()
